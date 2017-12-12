31620c34ed7b3a7cef4cde8876568b93fcefd7b0
nadleman <adleman@cua.edu>
2017-12-12 12:58:00 -0500

    adding bet skull stripping function

--- my_prepro.py
+++ my_prepro.py
@@ -16,10 +16,19 @@ import shutil
 
 def prepro(basedir):
     #do something cool
-    print ('Hello data in the path '+basedir)
+    for item in glob.glob(os.path.join(basedir,'sub-*','func','sub-*_task-bart_bold.nii.gz')):
+        input=item
+        output_path=item.split('.')[0]
+        output=output_path+('_brain')
+#        if os.path.exists(input)== True:
+#            print('cool!!!')
+#        else:
+#            print('yikes')
+#       pdb.set_trace()
+        os.system('bet %s %s -F'%(input,output))
+        
 def main():
     #load in all the global variables prepro needs, right now this is just the path to the data
     basedir='/Users/adleman/Downloads/data'
     prepro(basedir) #call prepro to do cool things
-
 main()
